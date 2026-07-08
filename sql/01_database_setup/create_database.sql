f979148cc7371ffaaead966e25d466b2cc3e406e
Anushka2234 <131282786+Anushka2234@users.noreply.github.com>
2026-07-08 11:36:28 +0530

    Update create_database.sql

--- sql/01_database_setup/create_database.sql
+++ sql/01_database_setup/create_database.sql
@@ -1,1 +1,44 @@
 
+/*
+====================================================
+
+Project:
+ShopSphere Enterprise Analytics Warehouse
+
+
+Script:
+Database Creation
+
+
+Purpose:
+
+Creates the main SQL Server database
+and required schemas.
+
+====================================================
+*/
+
+
+CREATE DATABASE ShopSphereDW;
+GO
+
+
+USE ShopSphereDW;
+GO
+
+
+
+CREATE SCHEMA raw;
+GO
+
+
+CREATE SCHEMA staging;
+GO
+
+
+CREATE SCHEMA dw;
+GO
+
+
+CREATE SCHEMA audit;
+GO
