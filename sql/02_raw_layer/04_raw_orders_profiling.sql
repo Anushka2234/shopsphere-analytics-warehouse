7acd145830a170dbbef4401339a9316f9de59223
Anushka2234 <131282786+Anushka2234@users.noreply.github.com>
2026-07-20 15:23:26 +0530

    Update 04_raw_orders_profiling.sql

--- sql/02_raw_layer/04_raw_orders_profiling.sql
+++ sql/02_raw_layer/04_raw_orders_profiling.sql
@@ -1,1 +1,48 @@
+--Total Records
+SELECT COUNT(*) AS total_orders
+FROM raw.orders;
 
+--Duplicate Orders
+SELECT
+    order_id,
+    COUNT(*) AS duplicate_count
+FROM raw.orders
+GROUP BY order_id
+HAVING COUNT(*) > 1;
+
+--NULL Analysis
+
+SELECT
+
+SUM(CASE WHEN order_id IS NULL THEN 1 ELSE 0 END) AS null_order_id,
+
+SUM(CASE WHEN customer_id IS NULL THEN 1 ELSE 0 END) AS null_customer_id,
+
+SUM(CASE WHEN order_status IS NULL THEN 1 ELSE 0 END) AS null_status,
+
+SUM(CASE WHEN order_purchase_timestamp IS NULL THEN 1 ELSE 0 END) AS null_purchase,
+
+SUM(CASE WHEN order_approved_at IS NULL THEN 1 ELSE 0 END) AS null_approved,
+
+SUM(CASE WHEN order_delivered_carrier_date IS NULL THEN 1 ELSE 0 END) AS null_carrier,
+
+SUM(CASE WHEN order_delivered_customer_date IS NULL THEN 1 ELSE 0 END) AS null_customer_delivery,
+
+SUM(CASE WHEN order_estimated_delivery_date IS NULL THEN 1 ELSE 0 END) AS null_estimated
+
+  
+--Order Status Check
+SELECT
+    order_status,
+    COUNT(*) AS total_orders
+FROM raw.orders
+GROUP BY order_status
+ORDER BY total_orders DESC;
+
+
+--Date Validation
+SELECT *
+FROM raw.orders
+WHERE order_purchase_timestamp >
+      order_estimated_delivery_date;
+FROM raw.orders;
